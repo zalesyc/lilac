cc6c8c8c924995567013e2297c04fdd39f99782d
Alex Čižinský <89998387+zalesyc@users.noreply.github.com>
2025-08-27 10:06:17 +0200

    Fix a crash, due to dereferencing a possibly hanging pointer

--- src/utils/slider_focus_frame.cpp
+++ src/utils/slider_focus_frame.cpp
@@ -2,6 +2,7 @@
 // SPDX-FileCopyrightText: 2025 zalesyc and the lilac contributors
 
 #include <QEvent>
+#include <QPointer>
 #include <QTimer>
 
 #include "slider_focus_frame.h"
@@ -12,7 +13,9 @@ SliderFocusFrame::~SliderFocusFrame() {}
 
 bool SliderFocusFrame::eventFilter(QObject* object, QEvent* event) {
     if (object == widget() && event->type() == QEvent::Paint) {
-        QTimer::singleShot(0, [this]() { this->update(); });
+        // the QPointer is requred to chech that this is not deleted before the lambda
+        QPointer<QFocusFrame> thisFrame = this;
+        QTimer::singleShot(0, [thisFrame]() {if (thisFrame) thisFrame->update(); });
         return false;
     }
     return QFocusFrame::eventFilter(object, event);
